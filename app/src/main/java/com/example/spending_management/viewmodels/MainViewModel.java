ab02c9d2112b60c4491ba129586089045100fdcd
trung-epicsoft4 <quoctrung@epicsoft4.com>
2024-11-29 17:41:20 +0700

    trung fix be

--- app/src/main/java/com/example/spending_management/viewmodels/MainViewModel.java
+++ app/src/main/java/com/example/spending_management/viewmodels/MainViewModel.java
@@ -18,11 +18,11 @@ import io.realm.RealmResults;
 public class MainViewModel extends AndroidViewModel {
 
     public MutableLiveData<RealmResults<Transaction>> transactions = new MutableLiveData<>();
-//    public MutableLiveData<RealmResults<Transaction>> categoriesTransactions = new MutableLiveData<>();
-//
-//    public MutableLiveData<Double> totalIncome = new MutableLiveData<>();
-//    public MutableLiveData<Double> totalExpense = new MutableLiveData<>();
-//    public MutableLiveData<Double> totalAmount = new MutableLiveData<>();
+    public MutableLiveData<RealmResults<Transaction>> categoriesTransactions = new MutableLiveData<>();
+
+    public MutableLiveData<Double> totalIncome = new MutableLiveData<>();
+    public MutableLiveData<Double> totalExpense = new MutableLiveData<>();
+    public MutableLiveData<Double> totalAmount = new MutableLiveData<>();
 
     Realm realm;
     Calendar calendar;
@@ -33,148 +33,147 @@ public class MainViewModel extends AndroidViewModel {
         setupDatabase();
     }
 
-//    public void getTransactions(Calendar calendar, String type) {
-//        this.calendar = calendar;
-//        calendar.set(Calendar.HOUR_OF_DAY, 0);
-//        calendar.set(Calendar.MINUTE, 0);
-//        calendar.set(Calendar.SECOND, 0);
-//        calendar.set(Calendar.MILLISECOND, 0);
-//
-//
-//        RealmResults<Transaction> newTransactions = null;
-//        if(Constants.SELECTED_TAB_STATS == Constants.DAILY) {
-//            newTransactions = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", calendar.getTime())
-//                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
-//                    .equalTo("type", type)
-//                    .findAll();
-//
-//        } else if(Constants.SELECTED_TAB_STATS == Constants.MONTHLY) {
-//            calendar.set(Calendar.DAY_OF_MONTH,0);
-//
-//            Date startTime = calendar.getTime();
-//
-//
-//            calendar.add(Calendar.MONTH,1);
-//            Date endTime = calendar.getTime();
-//
-//            newTransactions = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", startTime)
-//                    .lessThan("date", endTime)
-//                    .equalTo("type", type)
-//                    .findAll();
-//        }
-//
-//
-//        categoriesTransactions.setValue(newTransactions);
-//
-//    }
-//
-//
-//    public void getTransactions(Calendar calendar) {
-//        this.calendar = calendar;
-//        calendar.set(Calendar.HOUR_OF_DAY, 0);
-//        calendar.set(Calendar.MINUTE, 0);
-//        calendar.set(Calendar.SECOND, 0);
-//        calendar.set(Calendar.MILLISECOND, 0);
-//
-//        double income = 0;
-//        double expense = 0;
-//        double total = 0;
-//        RealmResults<Transaction> newTransactions = null;
-//        if(Constants.SELECTED_TAB == Constants.DAILY) {
-//            // Select * from transactions
-//            // Select * from transactions where id = 5
-//
-//            newTransactions = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", calendar.getTime())
-//                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
-//                    .findAll();
-//
-//            income = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", calendar.getTime())
-//                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
-//                    .equalTo("type", Constants.INCOME)
-//                    .sum("amount")
-//                    .doubleValue();
-//
-//            expense = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", calendar.getTime())
-//                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
-//                    .equalTo("type", Constants.EXPENSE)
-//                    .sum("amount")
-//                    .doubleValue();
-//
-//            total = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", calendar.getTime())
-//                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
-//                    .sum("amount")
-//                    .doubleValue();
-//
-//
-//
-//        } else if(Constants.SELECTED_TAB == Constants.MONTHLY) {
-//            calendar.set(Calendar.DAY_OF_MONTH,0);
-//
-//            Date startTime = calendar.getTime();
-//
-//
-//            calendar.add(Calendar.MONTH,1);
-//            Date endTime = calendar.getTime();
-//
-//            newTransactions = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", startTime)
-//                    .lessThan("date", endTime)
-//                    .findAll();
-//
-//            income = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", startTime)
-//                    .lessThan("date", endTime)
-//                    .equalTo("type", Constants.INCOME)
-//                    .sum("amount")
-//                    .doubleValue();
-//
-//            expense = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", startTime)
-//                    .lessThan("date", endTime)
-//                    .equalTo("type", Constants.EXPENSE)
-//                    .sum("amount")
-//                    .doubleValue();
-//
-//            total = realm.where(Transaction.class)
-//                    .greaterThanOrEqualTo("date", startTime)
-//                    .lessThan("date", endTime)
-//                    .sum("amount")
-//                    .doubleValue();
-//        }
-//
-//        totalIncome.setValue(income);
-//        totalExpense.setValue(expense);
-//        totalAmount.setValue(total);
-//        transactions.setValue(newTransactions);
-////        RealmResults<Transaction> newTransactions = realm.where(Transaction.class)
-////                .equalTo("date", calendar.getTime())
-////                .findAll();
-//
-//    }
-//
-//    public void addTransaction(Transaction transaction) {
-//        realm.beginTransaction();
-//        realm.copyToRealmOrUpdate(transaction);
-//        // some code here
-//        realm.commitTransaction();
-//    }
-//
-//    public void deleteTransaction(Transaction transaction) {
-//        realm.beginTransaction();
-//        transaction.deleteFromRealm();
-//        realm.commitTransaction();
-//        getTransactions(calendar);
-//    }
+    public void getTransactions(Calendar calendar, String type) {
+        this.calendar = calendar;
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
 
-    public void getTransactions(){
-        RealmResults<Transaction> transactions = realm.where(Transaction.class).findAll();
+
+        RealmResults<Transaction> newTransactions = null;
+        if(Constants.SELECTED_TAB == Constants.DAILY) {
+            newTransactions = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", calendar.getTime())
+                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
+                    .equalTo("type", type)
+                    .findAll();
+
+        } else if(Constants.SELECTED_TAB == Constants.MONTHLY) {
+            calendar.set(Calendar.DAY_OF_MONTH,0);
+
+            Date startTime = calendar.getTime();
+
+
+            calendar.add(Calendar.MONTH,1);
+            Date endTime = calendar.getTime();
+
+            newTransactions = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", startTime)
+                    .lessThan("date", endTime)
+                    .equalTo("type", type)
+                    .findAll();
+        }
+
+
+        categoriesTransactions.setValue(newTransactions);
+
     }
+
+
+    public void getTransactions(Calendar calendar) {
+        this.calendar = calendar;
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        double income = 0;
+        double expense = 0;
+        double total = 0;
+        RealmResults<Transaction> newTransactions = null;
+        if(Constants.SELECTED_TAB == Constants.DAILY) {
+            // Select * from transactions
+            // Select * from transactions where id = 5
+
+            newTransactions = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", calendar.getTime())
+                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
+                    .findAll();
+
+            income = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", calendar.getTime())
+                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
+                    .equalTo("type", Constants.INCOME)
+                    .sum("amount")
+                    .doubleValue();
+
+            expense = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", calendar.getTime())
+                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
+                    .equalTo("type", Constants.EXPENSE)
+                    .sum("amount")
+                    .doubleValue();
+
+            total = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", calendar.getTime())
+                    .lessThan("date", new Date(calendar.getTime().getTime() + (24 * 60 * 60 * 1000)))
+                    .sum("amount")
+                    .doubleValue();
+
+
+
+
+        } else if(Constants.SELECTED_TAB == Constants.MONTHLY) {
+            calendar.set(Calendar.DAY_OF_MONTH,0);
+
+            Date startTime = calendar.getTime();
+
+
+            calendar.add(Calendar.MONTH,1);
+            Date endTime = calendar.getTime();
+
+            newTransactions = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", startTime)
+                    .lessThan("date", endTime)
+                    .findAll();
+
+            income = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", startTime)
+                    .lessThan("date", endTime)
+                    .equalTo("type", Constants.INCOME)
+                    .sum("amount")
+                    .doubleValue();
+
+            expense = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", startTime)
+                    .lessThan("date", endTime)
+                    .equalTo("type", Constants.EXPENSE)
+                    .sum("amount")
+                    .doubleValue();
+
+            total = realm.where(Transaction.class)
+                    .greaterThanOrEqualTo("date", startTime)
+                    .lessThan("date", endTime)
+                    .sum("amount")
+                    .doubleValue();
+        }
+
+        totalIncome.setValue(income);
+        totalExpense.setValue(expense);
+        totalAmount.setValue(total);
+        transactions.setValue(newTransactions);
+        newTransactions = realm.where(Transaction.class)
+                .equalTo("date", calendar.getTime())
+                .findAll();
+
+    }
+
+    public void addTransaction(Transaction transaction) {
+        realm.beginTransaction();
+        realm.copyToRealmOrUpdate(transaction);
+        // some code here
+        realm.commitTransaction();
+    }
+
+    public void deleteTransaction(Transaction transaction) {
+        realm.beginTransaction();
+        transaction.deleteFromRealm();
+        realm.commitTransaction();
+        getTransactions(calendar);
+    }
+
+
 
     public void addTransactions() {
         realm.beginTransaction();
--- app/src/main/java/com/example/spending_management/views/fragments/AddTransactionFragment.java
+++ app/src/main/java/com/example/spending_management/views/fragments/AddTransactionFragment.java
@@ -21,8 +21,10 @@ import com.example.spending_management.databinding.FragmentAddTransactionBinding
 import com.example.spending_management.databinding.ListDialogBinding;
 import com.example.spending_management.models.Account;
 import com.example.spending_management.models.Category;
+import com.example.spending_management.models.Transaction;
 import com.example.spending_management.utils.Constants;
 import com.example.spending_management.utils.Helper;
+import com.example.spending_management.views.activities.MainActivity;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import java.text.SimpleDateFormat;
@@ -32,7 +34,7 @@ import java.util.Calendar;
 public class AddTransactionFragment extends BottomSheetDialogFragment {
 
     FragmentAddTransactionBinding binding;
-
+    Transaction transaction;
     public AddTransactionFragment() {
         // Required empty public constructor
     }
@@ -42,95 +44,121 @@ public class AddTransactionFragment extends BottomSheetDialogFragment {
         super.onCreate(savedInstanceState);
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         binding = FragmentAddTransactionBinding.inflate(inflater);
 
-        binding.incomeBtn.setOnClickListener(view ->{
+
+        transaction = new Transaction();
+
+        binding.incomeBtn.setOnClickListener(view -> {
             binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.income_selector));
             binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
             binding.expenseBtn.setTextColor(getContext().getColor(R.color.textColor));
             binding.incomeBtn.setTextColor(getContext().getColor(R.color.greenColor));
+
+            transaction.setType(Constants.INCOME);
         });
 
-        binding.expenseBtn.setOnClickListener(view ->{
+        binding.expenseBtn.setOnClickListener(view -> {
             binding.incomeBtn.setBackground(getContext().getDrawable(R.drawable.default_selector));
             binding.expenseBtn.setBackground(getContext().getDrawable(R.drawable.expense_selector));
             binding.incomeBtn.setTextColor(getContext().getColor(R.color.textColor));
             binding.expenseBtn.setTextColor(getContext().getColor(R.color.redColor));
+
+            transaction.setType(Constants.EXPENSE);
         });
 
         binding.date.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 DatePickerDialog datePickerDialog = new DatePickerDialog(getContext());
-                datePickerDialog.setOnDateSetListener((view, year, month, dayOfMonth) -> {
+                datePickerDialog.setOnDateSetListener((datePicker, i, i1, i2) -> {
                     Calendar calendar = Calendar.getInstance();
-                    calendar.set(Calendar.DAY_OF_MONTH, view.getDayOfMonth());
-                    calendar.set(Calendar.MONTH, view.getMonth());
-                    calendar.set(Calendar.YEAR, view.getYear());
+                    calendar.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
+                    calendar.set(Calendar.MONTH, datePicker.getMonth());
+                    calendar.set(Calendar.YEAR, datePicker.getYear());
 
                     //SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM, yyyy");
                     String dateToShow = Helper.formatDate(calendar.getTime());
 
                     binding.date.setText(dateToShow);
+
+                    transaction.setDate(calendar.getTime());
+                    transaction.setId(calendar.getTime().getTime());
                 });
                 datePickerDialog.show();
             }
         });
 
-        binding.category.setOnClickListener(c -> {
+        binding.category.setOnClickListener(c-> {
             ListDialogBinding dialogBinding = ListDialogBinding.inflate(inflater);
             AlertDialog categoryDialog = new AlertDialog.Builder(getContext()).create();
             categoryDialog.setView(dialogBinding.getRoot());
 
 
+
             CategoryAdapter categoryAdapter = new CategoryAdapter(getContext(), Constants.categories, new CategoryAdapter.CategoryClickListener() {
                 @Override
                 public void onCategoryClicked(Category category) {
-                    binding.category.setText(category.getCategory_name());
+                    binding.category.setText(category.getCategoryName());
+                    transaction.setCategory(category.getCategoryName());
                     categoryDialog.dismiss();
                 }
             });
-            dialogBinding.recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+            dialogBinding.recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
             dialogBinding.recyclerView.setAdapter(categoryAdapter);
 
             categoryDialog.show();
-
         });
 
-        binding.account.setOnClickListener(c -> {
+        binding.account.setOnClickListener(c-> {
             ListDialogBinding dialogBinding = ListDialogBinding.inflate(inflater);
-            AlertDialog accountDialog = new AlertDialog.Builder(getContext()).create();
-            accountDialog.setView(dialogBinding.getRoot());
+            AlertDialog accountsDialog = new AlertDialog.Builder(getContext()).create();
+            accountsDialog.setView(dialogBinding.getRoot());
 
             ArrayList<Account> accounts = new ArrayList<>();
             accounts.add(new Account(0, "Cash"));
             accounts.add(new Account(0, "Bank"));
-            accounts.add(new Account(0, "MB_Bank"));
-            accounts.add(new Account(0, "Viettel_Money"));
+            accounts.add(new Account(0, "PayTM"));
+            accounts.add(new Account(0, "EasyPaisa"));
             accounts.add(new Account(0, "Other"));
 
-            AccountAdapter accountAdapter = new AccountAdapter(getContext(), accounts, new AccountAdapter.AccountClickListener() {
+            AccountAdapter adapter = new AccountAdapter(getContext(), accounts, new AccountAdapter.AccountClickListener() {
                 @Override
                 public void onAccountSelected(Account account) {
-                    binding.account.setText(account.getAccount_name());
-                    accountDialog.dismiss();
+                    binding.account.setText(account. getAccount_name());
+                    transaction.setAccount(account. getAccount_name());
+                    accountsDialog.dismiss();
                 }
             });
             dialogBinding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
             //dialogBinding.recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
-            dialogBinding.recyclerView.setAdapter(accountAdapter);
+            dialogBinding.recyclerView.setAdapter(adapter);
 
-            accountDialog.show();
-
+            accountsDialog.show();
 
         });
 
+        binding.saveTransactionBtn.setOnClickListener(c-> {
+            double amount = Double.parseDouble(binding.amount.getText().toString());
+            String note = binding.note.getText().toString();
 
-        // Inflate the layout for this fragment
+            if(transaction.getType().equals(Constants.EXPENSE)) {
+                transaction.setAmount(amount*-1);
+            } else {
+                transaction.setAmount(amount);
+            }
+
+            transaction.setNote(note);
+
+            ((MainActivity)getActivity()).viewModel.addTransaction(transaction);
+            ((MainActivity)getActivity()).getTransactions();
+            dismiss();
+        });
+
         return binding.getRoot();
     }
 }

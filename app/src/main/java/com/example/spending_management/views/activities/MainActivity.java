b50ff670f297cde8abf653f0b048b7ad88de2abe
trung-epicsoft4 <quoctrung@epicsoft4.com>
2024-11-29 17:41:20 +0700

    trung fix be

--- app/src/main/java/com/example/spending_management/views/activities/MainActivity.java
+++ app/src/main/java/com/example/spending_management/views/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.spending_management.views.activities;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
@@ -16,6 +17,7 @@ import com.example.spending_management.viewmodels.MainViewModel;
 import com.example.spending_management.views.fragments.AddTransactionFragment;
 import com.example.spending_management.R;
 import com.example.spending_management.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabLayout;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -31,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     Calendar calendar;
 
-    Realm realm ;
 
-    MainViewModel viewModel ;
+    public MainViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,63 +44,127 @@ public class MainActivity extends AppCompatActivity {
 
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
+
+
         setSupportActionBar(binding.toolBar);
         getSupportActionBar().setTitle("Transactions");
+
+
+        Constants.setCategories();
+
+        calendar = Calendar.getInstance();
+        updateDate();
+
+        binding.nextDateBtn.setOnClickListener(c-> {
+            if(Constants.SELECTED_TAB == Constants.DAILY) {
+                calendar.add(Calendar.DATE, 1);
+            } else if(Constants.SELECTED_TAB == Constants.MONTHLY) {
+                calendar.add(Calendar.MONTH, 1);
+            }
+            updateDate();
+        });
+
+        binding.previousDateBtn.setOnClickListener(c-> {
+            if(Constants.SELECTED_TAB == Constants.DAILY) {
+                calendar.add(Calendar.DATE, -1);
+            } else if(Constants.SELECTED_TAB == Constants.MONTHLY) {
+                calendar.add(Calendar.MONTH, -1);
+            }
+            updateDate();
+        });
+
 
         binding.floatingActionButton.setOnClickListener(c -> {
             new AddTransactionFragment().show(getSupportFragmentManager(), null);
         });
 
 
-
-
-
-
-
-
-        binding.transactionList.setLayoutManager(new LinearLayoutManager(this));
-        viewModel.transactions.observe(this, new Observer<RealmResults<Transaction>>() {
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onChanged(RealmResults<Transaction> transactions) {
-                TransactionAdapter transactionAdapter = new TransactionAdapter(MainActivity.this, transactions);
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getText().equals("Monthly")) {
+                    Constants.SELECTED_TAB = 1;
+                    updateDate();
+                } else if(tab.getText().equals("Daily")) {
+                    Constants.SELECTED_TAB = 0;
+                    updateDate();
+                }
+            }
 
-                binding.transactionList.setAdapter(transactionAdapter);
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
 
 
 
-        Constants.setCategories();
-        calendar = Calendar.getInstance();
-        updateDate();
 
-        binding.previousDateBtn.setOnClickListener(c -> {
-            calendar.add(Calendar.DATE, -1);
-            updateDate();
+        binding.transactionList.setLayoutManager(new LinearLayoutManager(this));
+
+        viewModel.transactions.observe(this, new Observer<RealmResults<Transaction>>() {
+            @Override
+            public void onChanged(RealmResults<Transaction> transactions) {
+                TransactionAdapter transactionsAdapter = new TransactionAdapter(MainActivity.this, transactions);
+                binding.transactionList.setAdapter(transactionsAdapter);
+                if(transactions.size() > 0) {
+                    binding.emptyState.setVisibility(View.GONE);
+                } else {
+                    binding.emptyState.setVisibility(View.VISIBLE);
+                }
+            }
         });
 
-        binding.nextDateBtn.setOnClickListener(c -> {
-            calendar.add(Calendar.DATE, 1);
-            updateDate();
+        viewModel.totalIncome.observe(this, new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                binding.incomeLbl.setText(String.valueOf(aDouble));
+            }
         });
+
+        viewModel.totalExpense.observe(this, new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                binding.expenseLbl.setText(String.valueOf(aDouble));
+            }
+        });
+
+        viewModel.totalAmount.observe(this, new Observer<Double>() {
+            @Override
+            public void onChanged(Double aDouble) {
+                binding.totalLbl.setText(String.valueOf(aDouble));
+            }
+        });
+        viewModel.getTransactions(calendar);
+
 
 
     }
 
-    public  void setupDataBase(){
-        Realm.init(this);
-        realm =  Realm.getDefaultInstance();
+    public void getTransactions() {
+        viewModel.getTransactions(calendar);
+    }
+
+
+
+    void updateDate() {
+        if(Constants.SELECTED_TAB == Constants.DAILY) {
+            binding.currentDate.setText(Helper.formatDate(calendar.getTime()));
+        } else if(Constants.SELECTED_TAB == Constants.MONTHLY) {
+            binding.currentDate.setText(Helper.formatDateByMonth(calendar.getTime()));
+        }
+        viewModel.getTransactions(calendar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_menu, menu);
+        getMenuInflater().inflate(R.menu.top_menu,menu);
         return super.onCreateOptionsMenu(menu);
-    }
-
-    void updateDate(){
-        //SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM, yyyy");
-        binding.currentDate.setText(Helper.formatDate(calendar.getTime()));
     }
 }
